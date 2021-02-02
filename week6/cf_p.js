1f2c47a72f2e8d2490584a728d5d0f8b0c0f9b75
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-02-02 01:10:59 -0500

    Constructor and Prototype Mastery

--- week6/cf_p.js
+++ week6/cf_p.js
@@ -1,34 +1,37 @@
 function employee(name, jobtitle, salary, status = 'full-time'){
-    this.name = name;
-    this.jobtitle = jobtitle;
-    this.salary = salary;
-    this.status = status;
+        this.name = name;
+        this.jobtitle = jobtitle;
+        this.salary = salary;
+        this.status = status;
+//Traditional Approach
+//     this.printEmployeeForm = function(){
+//     console.log("Employee Name: " + this.name);
+//     console.log("Job Title: " + this.jobtitle);
+//     console.log("Salary: " + this.salary);
+//     console.log("Status: " + this.status);
+// }
+
 }
 
-
-function printEmployeeForm(employee){
-    console.log(employee.name + " Job Title: " + employee.jobtitle + " Salary: " + employee.salary + " Status: " + employee.status);
+//Prototype Approach
+employee.prototype.printEmployeeForm = function(){
+    console.log("Employee Name: " + this.name);
+    console.log("Job Title: " + this.jobtitle);
+    console.log("Salary: " + this.salary);
+    console.log("Status: " + this.status);
 }
 
 
 employee.prototype.statusOverride = "contract";
 
 var employee1 = new employee('Bob' , 'V School Instructor' , '$3000/hour');
-var employee2 = new employee('Jane' , 'Web Developer' , '$300/hour');
-var employee3 = new employee('Joe' , 'Auto-Mechanic' , '$30/hour' ,  employee.prototype.statusOverride);
-
-printEmployeeForm(employee1);
-printEmployeeForm(employee2);
-printEmployeeForm(employee3);
-
-
-console.log("----------------There's more than one way to do this-----------------")
-
-
-employee.prototype.printEmployeeForm = function(){
-    console.log(this.name + " Job Title: " + this.jobtitle + " Salary: " + this.salary + " Status: " + this.status);
-}
 employee1.printEmployeeForm();
-employee2.printEmployeeForm();
-employee3.printEmployeeForm();
+console.log("---------------------------------")
 
+var employee2 = new employee('Jane' , 'Web Developer' , '$300/hour');
+employee2.printEmployeeForm();
+console.log("---------------------------------")
+
+var employee3 = new employee('Joe' , 'Auto-Mechanic' , '$30/hour' ,  employee.prototype.statusOverride);
+employee3.printEmployeeForm();
+console.log("---------------------------------")
